6904d8ab51f31dc945c386e056a047adefeb2795
Fortune Mxoza <83515500+FortuneMx@users.noreply.github.com>
2021-05-19 14:55:02 +0200

    Add files via upload

---
+++ online rental portal/landlord_form.js
@@ -0,0 +1,102 @@
+var form1= document.getElementById("form1");
+var form2= document.getElementById("form2");
+var form3= document.getElementById("form3");
+var form4= document.getElementById("form4");
+var form5= document.getElementById("form5");
+var form6= document.getElementById("form6");
+var form7= document.getElementById("form7");
+
+
+var Next1= document.getElementById("Next1");
+var Next2= document.getElementById("Next2");
+var Next3= document.getElementById("Next3");
+var Next4= document.getElementById("Next4");
+var Next5= document.getElementById("Next5");
+var Next6= document.getElementById("Next6");
+var Next7= document.getElementById("Next7");
+
+var Back1= document.getElementById("Back1");
+var Back2= document.getElementById("Back2");
+var Back3= document.getElementById("Back3");
+var Back4= document.getElementById("Back4");
+var Back5= document.getElementById("Back5");
+var Back6= document.getElementById("Back6");
+
+
+
+Next1.onclick=function(){
+    form1.style.left="-700px";
+    form2.style.left="20px";
+    
+}
+
+Back1.onclick=function(){
+    form1.style.left="20px";
+    form2.style.left="700px";
+    
+}
+
+Next2.onclick=function(){
+    form2.style.left="-700px";
+    form3.style.left="20px";
+    
+}
+
+Back2.onclick=function(){
+    form2.style.left="20px";
+    form3.style.left="700px";
+    
+}
+
+Next3.onclick=function(){
+    form3.style.left="-700px";
+    form4.style.left="20px";
+    
+}
+
+Back3.onclick=function(){
+    form3.style.left="20px";
+    form4.style.left="700px";
+    
+}
+
+Next4.onclick=function(){
+    form4.style.left="-700px";
+    form5.style.left="20px";
+    
+}
+
+Back4.onclick=function(){
+    form4.style.left="20px";
+    form5.style.left="700px";
+    
+}
+
+Next5.onclick=function(){
+    form5.style.left="-700px";
+    form6.style.left="20px";
+    
+}
+
+Back5.onclick=function(){
+    form5.style.left="20px";
+    form6.style.left="700px";
+    
+}
+
+Next6.onclick=function(){
+    form6.style.left="-700px";
+    form7.style.left="20px";
+    
+}
+
+Back6.onclick=function(){
+    form6.style.left="20px";
+    form7.style.left="700px";
+    
+}
+
+Next7.onclick=function(){
+    
+}
+
